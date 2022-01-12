240fe3a07d88bee17a6afa52b3527feeec129802
Robbendebiene <robbendebiene@mailbox.org>
2022-01-12 17:02:57 +0100

    Export more internal classes

--- lib/osm_api.dart
+++ lib/osm_api.dart
@@ -4,13 +4,20 @@ import '/src/osm_apis/osm_changeset_api_calls.dart';
 import '/src/osm_apis/osm_element_api_calls.dart';
 import '/src/osm_apis/osm_api_base.dart';
 
+export '/src/authentication/auth.dart';
 export '/src/authentication/basic_auth.dart';
 export '/src/authentication/oauth_auth.dart';
 export '/src/authentication/oauth2_auth.dart';
+
 export '/src/commons/bounding_box.dart';
 export '/src/commons/osm_exceptions.dart';
-export '/src/osm_user/osm_permissions.dart';
+
 export '/src/osm_elements/osm_elements.dart';
+
+export '/src/osm_user/osm_user.dart';
+export '/src/osm_user/osm_user_details.dart';
+export '/src/osm_user/osm_user_private_details.dart';
+export '/src/osm_user/osm_permissions.dart';
 
 /**
  * A super class that contains all API calls for sending requests to an OSM API server.
